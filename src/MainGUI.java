441d392adfe937c93f8fae2851fc67487276cfd4
BT_4N2_17 <zwu2542@bths.edu>
2023-05-31 12:12:55 -0400

    Figure out how to make threads, probably 4 so that each arrow will fall down instead of reading a bunch of code.

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -5,27 +5,28 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class MainGUI extends JPanel {
+public class MainGUI {
     public MainGUI() throws IOException {
         JFrame frame = buildFrame();
-        BufferedImage image = ImageIO.read(new File("C:\\Users\\wzixu\\IdeaProjects\\Mania\\src\\Down.png"));
-        image = new BufferedImage(50,50,BufferedImage.TYPE_INT_RGB);
-        BufferedImage finalImage = image;
-        JPanel pane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(finalImage, 50, 50, null);
-            }
-        };
-        frame.add(pane);
+        JPanel panel = new NewPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+
     }
     public static JFrame buildFrame(){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocation(0,0);
-        frame.setSize(1920, 1080);
+        frame.setLocation(720,0);
+        frame.setSize(500, 1080);
         frame.setVisible(true);
         return frame;
+    }
+    public static BufferedImage resize(BufferedImage img,int newW,int newH){
+        Image temp = img.getScaledInstance(newW,newH,Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW,newH,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(temp,0,0,null);
+        g2d.dispose();
+        return dimg;
     }
 }
